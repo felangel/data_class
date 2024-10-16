ce20a893fdc1283a71e627e75ad133ea3d2c367d
Felix Angelov <felangelov@gmail.com>
2024-10-15 22:07:04 -0500

    chore: remove unused import

--- lib/src/_data_class.dart
+++ lib/src/_data_class.dart
@@ -1,5 +1,3 @@
-import 'package:collection/collection.dart';
-
 // Libraries used in augmented code.
 final dartCore = Uri.parse('dart:core');
 final dataClassMacro = Uri.parse('package:data_class/src/_data_class.dart');
