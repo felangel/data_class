3e3c59e35d9c1d4f84f3bfff83c4eb749c9fcc3c
Felix Angelov <felangelov@gmail.com>
2024-10-15 21:15:00 -0500

    chore: use `pkg:equatable` (#41)

--- lib/data_class.dart
+++ lib/data_class.dart
@@ -1,8 +1,9 @@
 /// Experimental support for data classes in Dart using package:macros.
 library data_class;
 
+export 'package:equatable/equatable.dart' show Equatable;
+
 export 'src/constructable_macro.dart' show Constructable;
 export 'src/copyable_macro.dart' show Copyable;
 export 'src/data_macro.dart' show Data;
-export 'src/equatable_macro.dart' show Equatable;
 export 'src/stringable_macro.dart' show Stringable;
